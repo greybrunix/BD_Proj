7a5e5957e3ed6f8e3751978184b1a74a5b391b8e
Bruno Gião <a96544@alunos.uminho.pt>
2023-12-05 00:51:20 +0000

    proc

--- sql_scripts/queries/query.sql
+++ sql_scripts/queries/query.sql
@@ -1,30 +1,87 @@
--- Consultar qual funcionário gere qual
+-- check who manages an employee
 DELIMITER &&
-CREATE PROCEDURE GetFuncGere (IN employee_id_e VARCHAR(10))
+CREATE PROCEDURE GetMngr (IN id VARCHAR(10))
  BEGIN
-	DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTERED' Message;
-    SELECT *
-		FROM employee AS E
-			WHERE E.employee_id_e = employee_id_e;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTERED' Message;
+    SELECT employee_id_e
+        FROM employee AS E
+	WHERE id = E.id;
+END &&
+-- check who an employee manages
+DELIMITER &&
+CREATE PROCEDURE GetMngd (IN id VARCHAR(10))
+  BEGIN
+     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTERED' Message;
+     SELECT *
+        FROM employee AS E
+	   WHERE E.employee_id_e = id;
 END &&
 
--- Consultar as vendas efetuadas por um funcionário
+-- check on employee sales
 DELIMITER &&
-CREATE PROCEDURE GetVendasFunc (IN employee_id_e VARCHAR(10))
+CREATE PROCEDURE GetVendasFunc (IN id VARCHAR(10))
  BEGIN
-	DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTERED' Message;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTERED' Message;
     SELECT *
-		FROM employee AS E INNER JOIN sale as S
-			ON E.employee_id_e = S.employee_id_s
-		WHERE E.employee_id_e = employee_id_e;
+        FROM employee AS E INNER JOIN sale as S
+            ON E.id = S.id
+        WHERE E.id = id;
 END &&
-
--- Consultar todas as vendas efetuadas
+-- check all sales
 DELIMITER &&
 CREATE PROCEDURE GetVendas ()
  BEGIN
-	DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTERED' Message;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTERED' Message;
     SELECT *
-		FROM sale;
+        FROM sale;
 END &&
 
+-- check articles in sale
+DELIMITER &&
+CREATE PROCEDURE GetProductsInSale (IN id INTEGER)
+ BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTER' Message;
+    SELECT product_id_sp
+        FROM sale_product
+	WHERE sale_id_sp = id;
+END &&
+-- check all participants of a given event
+-- DELIMITER &&
+-- CREATE PROCEDURE GetEvParticipants (IN id INTEGER)
+--   BEGIN
+--      DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTER' Message;
+--      SELECT id
+--          FROM sale AS S INNER JOIN 
+--          WHERE S.participant_id_s = id AND 
+-- S.participant_id_s <-> S.employee_id_s <-> event_employee.employee_id_ee
+--END &&
+-- check all participants of all events
+-- DELIMITER &&
+-- CREATE PROCEDURE GetEvParticipants (IN id INTEGER)
+--   BEGIN
+--      DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTER' Message;
+--      SELECT id
+--          FROM sale AS S INNER JOIN 
+--          WHERE S.participant_id_s = id AND 
+-- S.participant_id_s <-> S.employee_id_s <-> event_employee.employee_id_ee
+--END &&
+-- check participant associated with a specific sale
+DELIMITER &&
+CREATE PROCEDURE GetPartInSale (IN id INTEGER)
+  BEGIN 
+     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUTER' Message;
+     SELECT id
+         FROM sale
+	 WHERE participant_id_s = id;
+END &&
+-- check all products
+DELIMITER &&
+CREATE PROCEDURE GetProds ()
+  BEGIN 
+     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUTER' Message;
+     SELECT *
+         FROM product;
+END &&
+--
+-- 
+>>>>>>> adb848a (proc)
