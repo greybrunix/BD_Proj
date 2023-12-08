3f58d241fadaaa44cc2ee8ce19d23f0165bc6b43
Bruno Gião <a96544@alunos.uminho.pt>
2023-12-08 00:25:11 +0000

    ad

--- sql_scripts/func_procs_queries/updates.sql
+++ sql_scripts/func_procs_queries/updates.sql
@@ -29,33 +29,33 @@ END &&
 
 DELIMITER &&
 CREATE PROCEDURE register_sale (IN p_id INTEGER,
-       	s_val DECIMAL(5,2),
-	    s_quant INTEGER,
-       	s_date DATETIME,
-	    e_id VARCHAR(10))
+      s_val DECIMAL(5,2),
+      s_quant INTEGER,
+      s_date DATETIME,
+      e_id VARCHAR(10))
   BEGIN
   -- update sale table
-  INSERT INTO sale(val, quantity, dos, employee_id_s, participant_id_s)
-	VALUES (s_val, s_quant, s_date, e_id, p_id);
+        INSERT INTO sale(val, quantity, dos, employee_id_s, participant_id_s)
+         VALUES (s_val, s_quant, s_date, e_id, p_id);
 END &&
 
 DELIMITER &&
 CREATE PROCEDURE register_new_employee (IN e_id VARCHAR(10), e_name VARCHAR(75),
-       	e_vat VARCHAR(9), e_birth   DATE, e_street   VARCHAR(50),
-       	e_locale   VARCHAR(30), e_postal   VARCHAR(15), e_employee_id_e   VARCHAR(10))
+       e_vat VARCHAR(9), e_birth   DATE, e_street   VARCHAR(50),
+       e_locale   VARCHAR(30), e_postal   VARCHAR(15), e_employee_id_e   VARCHAR(10))
   BEGIN
     -- update employee table
-	INSERT INTO employee (id, name, vat, birth, street, locale, postal, employee_id_e)
-	VALUES (e_id,e_name,e_vat,e_birth,e_street,e_locale,e_postal,e_employee_id_e);
+        INSERT INTO employee (id, name, vat, birth, street, locale, postal, employee_id_e)
+        VALUES (e_id,e_name,e_vat,e_birth,e_street,e_locale,e_postal,e_employee_id_e);
 END &&
 
 DELIMITER &&
 CREATE PROCEDURE register_new_event (IN e_descr TEXT,
-	e_beg DATETIME, e_fin DATETIME, e_capacity INTEGER)
+        e_beg DATETIME, e_fin DATETIME, e_capacity INTEGER)
   BEGIN
     -- update with new event
-	INSERT INTO event (descr, beg, fin, capacity)
-	VALUES (e_descr, e_beg, e_fin , e_capacity);
+        INSERT INTO event (descr, beg, fin, capacity)
+        VALUES (e_descr, e_beg, e_fin , e_capacity);
 END &&
 
 
