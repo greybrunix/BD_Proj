0734dbd8dc7a6a011ceb5c35685f63f9fa050ee3
Bruno Gião <a96544@alunos.uminho.pt>
2023-12-08 15:20:11 +0000

    report

--- sql_scripts/func_procs_queries/updates.sql
+++ sql_scripts/func_procs_queries/updates.sql
@@ -1,22 +1,30 @@
 USE mademoiselle_borges
 
 DELIMITER &&
-CREATE PROCEDURE register_delivery_new_product (IN p_NAME VARCHAR(75),
-       p_descr TEXT, p_price DECIMAL(5,2), p_stock INT, s_id INT, p_dod DATETIME,
+CREATE PROCEDURE register_reservation_new_product (IN p_NAME VARCHAR(75),
+       p_descr TEXT, p_price DECIMAL(5,2), s_id INTEGER, p_dod DATETIME,
        p_quantity INT)
   BEGIN
         -- update the product table
         INSERT INTO product(name, descr, price, stock)
-        VALUES(p_NAME, p_descr, p_price, p_stock);
+        VALUES(p_NAME, p_descr, p_price, "0");
 
-        -- update the the past supplies table
-        INSERT INTO product_supplier_past(product_id_psp, supplier_id_psp,dod, quantity)
+        -- update the the future supplies table
+        INSERT INTO product_supplier_future(product_id_psp, supplier_id_psp,dod, quantity)
         VALUES(LAST_INSERT_ID(), s_id, p_dod, p_quantity);
 END &&
 
 DELIMITER &&
-CREATE PROCEDURE register_delivery_exis_product (IN p_id INTEGER,
-       p_stock INT, s_id INT, p_dod DATETIME, p_quantity INT)
+CREATE PROCEDURE register_reservation_exis_product (IN p_id INTEGER,
+       p_stock INTEGER, s_id INTEGER, p_dod DATETIME, p_quantity INTEGER)
+  BEGIN
+        -- update the the future supplies table
+        INSERT INTO product_supplier_future(product_id_psp, supplier_id_psp,dod, quantity)
+        VALUES(p_id, s_id, p_dod, p_quantity);
+END &&
+DELIMITER &&
+CREATE PROCEDURE register_delivery_product (IN p_id INTEGER,
+       p_stock INTEGER, s_id INTEGER, p_dod DATETIME, p_quantity INTEGER)
   BEGIN
         -- update the remainding stock of the given product
         UPDATE product
@@ -25,18 +33,26 @@ CREATE PROCEDURE register_delivery_exis_product (IN p_id INTEGER,
         -- 
         INSERT INTO product_supplier_past(product_id_psp, supplier_id_psp,dod, quantity)
         VALUES(LAST_INSERT_ID(), s_id, p_dod, p_quantity);
+
+	UPDATE product_supplier_future
+	SET is_del = True
+		WHERE product_id_psp = p_id 
+		AND supplier_id_psp = s_id 
+		AND dod = p_dod;
 END &&        
 
 DELIMITER &&
 CREATE PROCEDURE register_sale (IN p_id INTEGER,
-      s_val DECIMAL(5,2),
-      s_quant INTEGER,
       s_date DATETIME,
       e_id VARCHAR(10))
   BEGIN
   -- update sale table
+	DECLARE s_totval DECIMAL(5,2);
+	DECLARE s_totquant INTEGER;
+	SELECT val INTO 
         INSERT INTO sale(val, quantity, dos, employee_id_s, participant_id_s)
-         VALUES (s_val, s_quant, s_date, e_id, p_id);
+         VALUES (s_totval, s_totquant, s_date, e_id, p_id);
+	
 END &&
 
 DELIMITER &&
@@ -58,4 +74,7 @@ CREATE PROCEDURE register_new_event (IN e_descr TEXT,
         VALUES (e_descr, e_beg, e_fin , e_capacity);
 END &&
 
-
+DELIMITER &&
+CREATE PROCEDURE update_info____()
+  BEGIN
+END &&
