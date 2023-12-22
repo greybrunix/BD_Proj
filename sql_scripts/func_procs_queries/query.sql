805f97af2dc3b15f26f00b2006bcd4bb8b9c0df9
Bruno Gião <a96544@alunos.uminho.pt>
2023-12-22 18:17:30 +0000

    Outlined all queries with corresponding requirement

--- sql_scripts/func_procs_queries/query.sql
+++ sql_scripts/func_procs_queries/query.sql
@@ -1,7 +1,7 @@
 USE mademoiselle_borges;
 
 
--- check who manages an employee (41)
+-- check who manages an employee (43)
 DELIMITER &&
 CREATE FUNCTION check_manager (id VARCHAR(10))
 RETURNS VARCHAR(10)
@@ -14,45 +14,24 @@ DETERMINISTIC
     RETURN manager;
 END &&
 
--- check who an employee manages (RM35)
-DELIMITER &&
-CREATE PROCEDURE check_managed_by (IN id VARCHAR(10))
-  BEGIN
-     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTERED' Message;
-     SELECT *
-        FROM employee AS E
-        WHERE E.employee_id_e = id;
-END &&
-
--- check on employee sales (RM02)
-DELIMITER &&
-CREATE PROCEDURE check_employee_sales (IN id VARCHAR(10))
- BEGIN 
-    SELECT *
-        FROM employee AS E INNER JOIN sale as S
-            ON E.id = S.employee_id_s
-        WHERE E.id = id;
-END &&
-
--- check all sales (RM05)
-SELECT *
-	FROM sale;
-
--- check articles in sale (RM03)
+-- check products in sale (44)
 DELIMITER &&
 CREATE PROCEDURE check_products_in_sale (IN id INTEGER)
  BEGIN
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTER' Message;
     SELECT product_id_sp
         FROM sale_product
         WHERE sale_id_sp = id;
 END &&
 
--- check all participants of a given event (RM06)
+-- check all closed sales (45)
+SELECT id
+	FROM sale
+	WHERE dos IS NOT NULL;
+
+-- check all participants of a given event (46)
 DELIMITER &&
-CREATE PROCEDURE GetEvParticipants (IN name VARCHAR(75))
+CREATE PROCEDURE check_parts_in_ev (IN name VARCHAR(75))
   BEGIN
-      DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTER' Message;
       SELECT P.id, P.name
           FROM participant AS P INNER JOIN sale as S
 			ON P.id = S.participant_id_s
@@ -64,8 +43,7 @@ CREATE PROCEDURE GetEvParticipants (IN name VARCHAR(75))
 			ON EE.event_id_ee = EV.id
 		  WHERE EV.name = name;
 END &&
-
--- check all participants of all events (RM07)
+-- check all participants of all events (47)
 SELECT P.id, P.name
   FROM participant AS P INNER JOIN sale as S
 		ON P.id = S.participant_id_s
@@ -75,9 +53,12 @@ SELECT P.id, P.name
 		ON E.id = EE.employee_id_ee
 	  INNER JOIN event as EV
 		ON EE.event_id_ee = EV.id;
-END &&
 
--- check participant associated with a specific sale (RM08)
+-- check all participants of all events (47)
+SELECT id, name
+	FROM participant;
+
+-- check participant associated with a specific sale (48)
 DELIMITER &&
 CREATE FUNCTION check_participant_in_sale (id INTEGER)
 RETURNS INTEGER
@@ -90,62 +71,74 @@ DETERMINISTIC
      RETURN partic;
 END &&
 
--- check all products (RM09)
-SELECT *
+-- check all products (49)
+SELECT id, name
  FROM product;
 
--- check all sales associated with a participant (RM11)
+-- check suppliers of a product (50)
+DELIMITER &&
+CREATE PROCEDURE check_product_suppliers (IN id INTEGER) 
+	BEGIN
+     SELECT PSP.id, PSP.name, PSF.id, PSF.name
+         FROM product_supplier_past AS PSP INNER JOIN product_supplier_future AS PSF
+			ON PSP.product_id_psp = id AND PSF.product_id_psf = id;
+END &&
+
+-- check past suppliers of a product (51)
+DELIMITER &&
+CREATE PROCEDURE check_past_suppliers_prod (IN id INTEGER)
+	BEGIN
+	SELECT PSP.id, PSP.name
+		FROM product_supplier_past AS PSP
+		WHERE PSP.id = id;
+END &&
+
+-- check future supppliers of a product (52)
+DELIMITER &&
+CREATE PROCEDURE check_fut_suppliers_prod (IN id INTEGER)
+	BEGIN
+	SELECT PSF.id, PSP.name
+		FROM product_supplier_future AS PSF
+		WHERE PSF.id = id;
+END &&
+
+-- check all sales associated with a participant (53)
 DELIMITER &&
 CREATE PROCEDURE check_all_participant_sales (IN id INTEGER)
   BEGIN
-     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTER' Message;
      SELECT *
          FROM sale AS S
          WHERE S.participant_id_s = id;
 END &&
 
--- check participant with most sales associated (RM16)
-SELECT P.id, P.name
-	FROM participant as P INNER JOIN sale as S
-		ON P.id = S.participant_id_s
-	GROUP BY P.id
-		ORDER BY COUNT(P.id = S.participant_id_s) DESC
-LIMIT 1;
-
--- check all suppliers (RM12)
-SELECT *
+-- check all suppliers (54)
+SELECT id, name
  FROM supplier;
+-- check all employees (55)
+SELECT id, name
+	FROM employee;
+-- check all events (56)
+SELECT id, name
+	FROM event;
 
--- check suppliers of a product (RM10)
-DELIMITER &&
-CREATE PROCEDURE check_product_suppliers (IN id INTEGER) 
-  BEGIN
-     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTER' Message;
-     SELECT *
-         FROM product_supplier_past AS PSP INNER JOIN product_supplier_future AS PSF
-			ON PSP.product_id_psp = id AND PSF.product_id_psf = id;
-END &&
-
--- check sales in a given day (RM15)
+-- check sale values and volume in a given day (57 and 58)
 DELIMITER &&
 CREATE PROCEDURE check_daily_sales (IN dos DATE)
   BEGIN
-     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQL EXCEPTION ENCOUNTER' Message;
-     SELECT *
+     SELECT SUM(S.value) AS Value, SUM(S.quantity) AS Volume
          FROM sale AS S
          WHERE YEAR(S.dos) = YEAR(dos) AND MONTH(S.dos) = MONTH(dos) AND DAY(S.dos) = DAY(dos);
 END &&
 
--- check the event with the most value in sales (RM37) INCORRETO
-
-SELECT E.id, E.name, SUM(S.Val) AS totVal
-	FROM event AS E INNER JOIN sale as S
-		ON S.dos BETWEEN E.beg AND E.fin
-	GROUP BY E.id, E.name
-		ORDER BY totVal DESC
+-- check participant with highest volume sales associated (59)
+SELECT P.id, P.name, SUM(S.quantity) AS Volume
+	FROM participant as P INNER JOIN sale as S
+		ON P.id = S.participant_id_s
+	GROUP BY P.id
+		ORDER BY Volume DESC --COUNT(P.id = S.participant_id_s) DESC
 LIMIT 1;
 
--- CHECK which event has most volume sales
+-- check which event has most volume sales (60)
 SELECT EV.id, EV.name, SUM(SP.quantity) AS quant
 	FROM event AS EV INNER JOIN product AS P
 		ON EV.name = P.name
@@ -154,7 +147,11 @@ SELECT EV.id, EV.name, SUM(SP.quantity) AS quant
 	GROUP BY EV.id, EV.name
 		ORDER BY quant DESC
 LIMIT 1;
--- check who sold the most tickets in Event
+
+-- check event with highest rate participation (61)
+
+-- check events in a given timespan (71)
+-- check who sold the most tickets in Event (72)
 DELIMITER &&
 CREATE FUNCTION GetSoldMostInEv (id INTEGER)
 RETURNS VARCHAR(10)
@@ -170,8 +167,37 @@ DETERMINISTIC
 				ON Em.id = S.employee_id_s
 		GROUP BY Ev.id, Ev.name
 			ORDER BY SUM(S.quantity);
-	-- LIMIT 1;
+	LIMIT 1;
     RETURN res;
+END &&
+-- FUNCTION????
+
+-- events someone participated in (81)
+
+-- check who an employee manages (92)
+DELIMITER &&
+CREATE PROCEDURE check_managed_by (IN id VARCHAR(10))
+  BEGIN
+     SELECT *
+        FROM employee AS E
+        WHERE E.employee_id_e = id;
+END &&
+
+-- check the event with the most value in sales (93)
+SELECT E.id, E.name, SUM(S.Val) AS totVal
+	FROM event AS E INNER JOIN sale as S
+		ON S.dos BETWEEN E.beg AND E.fin
+	GROUP BY E.id, E.name
+		ORDER BY totVal DESC
+LIMIT 1;
+
+-- check on employee sales (98)
+DELIMITER &&
+CREATE PROCEDURE check_employee_sales (IN id VARCHAR(10))
+ BEGIN 
+    SELECT S.id
+        FROM sale
+        WHERE employee_id_s = id;
 END &&
 
 DROP FUNCTION GetSoldMostInEv;
