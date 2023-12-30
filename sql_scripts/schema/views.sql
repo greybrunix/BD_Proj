ed8de5ff226eeaf1f739b18454c8df071ec5ad87
Pereirics <joao.lc.pereira1@gmail.com>
2023-12-30 23:21:36 +0000

    update

--- sql_scripts/schema/views.sql
+++ sql_scripts/schema/views.sql
@@ -1,17 +1,26 @@
-CREATE VIEW total_sale_value AS
+-- (65)
+CREATE VIEW total_event_sale_value AS
 	SELECT EV.id AS event_id, SUM(S.val) AS total_value
 		FROM event AS EV INNER JOIN sale AS S
 			ON S.dos BETWEEN EV.beg AND EV.fin
 	GROUP BY EV.id;
 
+-- (78)
 CREATE VIEW past_events AS
 	SELECT * FROM event AS EV
 		WHERE EV.fin < CURDATE();
-        
+
+-- (79)
 CREATE VIEW ongoing_events AS
 	SELECT * FROM event AS EV
 		WHERE CURDATE() BETWEEN EV.beg AND EV.fin;
-        
+       
+-- (80)
 CREATE VIEW future_events AS
 	SELECT * FROM event AS EV
 		WHERE CURDATE() < EV.beg;
+        
+-- (89)
+CREATE VIEW total_sale_value AS
+	SELECT SUM(S.val) AS total_value
+		FROM sale AS S;
