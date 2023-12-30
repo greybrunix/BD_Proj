42fa5eeb4a93a1c307f6e9c3e698fa38b00cc8fc
Pereirics <joao.lc.pereira1@gmail.com>
2023-12-30 22:34:15 +0000

    some views

--- sql_scripts/schema/views.sql
+++ sql_scripts/schema/views.sql
@@ -1,7 +1,17 @@
-USE mademoiselle_borges;
+CREATE VIEW total_sale_value AS
+	SELECT EV.id AS event_id, SUM(S.val) AS total_value
+		FROM event AS EV INNER JOIN sale AS S
+			ON S.dos BETWEEN EV.beg AND EV.fin
+	GROUP BY EV.id;
 
-CREATE VIEW announced_event AS
-SELECT *
-	FROM event
-	WHERE ;
-
+CREATE VIEW past_events AS
+	SELECT * FROM event AS EV
+		WHERE EV.fin < CURDATE();
+        
+CREATE VIEW ongoing_events AS
+	SELECT * FROM event AS EV
+		WHERE CURDATE() BETWEEN EV.beg AND EV.fin;
+        
+CREATE VIEW future_events AS
+	SELECT * FROM event AS EV
+		WHERE CURDATE() < EV.beg;
