ebbb54a31ab8dc315247e87c8c8fe46a84ca384b
Bruno Gião <a96544@alunos.uminho.pt>
2024-01-12 15:25:24 +0000

    grant

--- sql_scripts/schema/access.sql
+++ sql_scripts/schema/access.sql
@@ -3,24 +3,17 @@ USE mademoiselle_borges;
 CREATE USER 'hen'@'localhost';
 CREATE USER 'mii'@'localhost';
 CREATE USER 'hom'@'localhost';
-
 CREATE USER 'employee'@'localhost';
 CREATE USER 'marketing'@'localhost';
-
 CREATE USER 'guest'@'localhost';
 
-DROP USER 'hen';
-DROP USER 'mii';
-DROP USER 'hom';
-DROP USER 'employee';
-DROP USER 'guest'@'localhost';
+GRANT SELECT ON mademoiselle_borges.EventCal TO 'guest'@'localhost';
+GRANT  ALL PRIVILEGES ON mademoiselle_borges.* TO 'hen'@'localhost';
+GRANT  GRANT OPTION ON mademoiselle_borges.* TO 'hen'@'localhost';
+GRANT ALL PRIVILEGES ON mademoiselle_borges.* TO 'mii'@'localhost';
+GRANT  GRANT OPTION ON mademoiselle_borges.* TO 'mii'@'localhost';
+GRANT ALL PRIVILEGES  ON mademoiselle_borges.* TO 'hom'@'localhost';
+GRANT GRANT OPTION ON mademoiselle_borges.* TO 'hom'@'localhost';
 
-GRANT SELECT ON EventCal TO 'guest'@'localhost';
-GRANT ALL ON * TO 'hen'@'localhost';
-GRANT ALL ON * TO 'mii'@'localhost';
-GRANT ALL ON * TO 'hom'@'localhost';
-
-
-
-SHOW GRANTS;
+SHOW GRANTS FOR 'guest'@'localhost';
 FLUSH PRIVILEGES;
