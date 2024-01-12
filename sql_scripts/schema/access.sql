49ab785440f669ae890af5ccfc077b1337a96e84
Bruno Gião <a96544@alunos.uminho.pt>
2024-01-12 15:02:50 +0000

    grant

--- sql_scripts/schema/access.sql
+++ sql_scripts/schema/access.sql
@@ -1,12 +1,14 @@
 USE mademoiselle_borges;
 
-CREATE USER hen; -- Henrique Borges
-CREATE USER mii; -- MARIA IVANOVNA IVANOVA
-CREATE USER hom; -- Herr Otto Mustermann
+CREATE USER hen;
+CREATE USER mii;
+CREATE USER hom;
 
 CREATE USER employee;
 
-CREATE USER guest; -- global
+CREATE USER guest;
 
 
+GRANT DENY ALL ON * TO guest;
 GRANT SELECT ON EventCal TO guest;
+GRANT ALL ON * TO hen;
